a192358b486584ffe9cd2fea9fc41f7efeb501f8
Sherlocky <y836097668@126.com>
2019-07-10 14:23:12 +0800

    使用Lambda andThen() 简单实现责任链模式

---
+++ src/main/java/com/sherlocky/learning/java8/lambda/LambdaSuccessorSample.java
@@ -0,0 +1,33 @@
+package com.sherlocky.learning.java8.lambda;
+
+import strman.Strman;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+/**
+ * 使用Lambda andThen() 简单实现责任链模式
+ *
+ * @author: zhangcx
+ * @date: 2019/7/10 14:12
+ */
+public class LambdaSuccessorSample {
+    public static void main(String[] args) {
+        /**
+         * 函数式编程思维
+         */
+        UnaryOperator<String> headerProcessing = (String text) -> {
+            return "Someone said: " + text;
+        };
+        UnaryOperator<String> spellCheckProcessing = text -> text.replace("labda", "lambda");
+
+        Function<String, String> function = headerProcessing.andThen(spellCheckProcessing);
+
+        String src = "Aren't labdas really sexy?!!";
+        System.out.println(function.apply(src));
+
+        System.out.println(function.andThen(text -> {
+            return Strman.surround(text, "【", "】");
+        }).apply(src));
+    }
+}
