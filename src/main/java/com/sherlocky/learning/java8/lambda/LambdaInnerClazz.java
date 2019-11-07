481c235e8b2356a9b12e6acd4603080e4a578a6f
Sherlock <y836097668@126.com>
2019-11-07 12:01:30 +0800

    Lambda VS 内部类补充说明

--- src/main/java/com/sherlocky/learning/java8/lambda/LambdaInnerClazz.java
+++ src/main/java/com/sherlocky/learning/java8/lambda/LambdaInnerClazz.java
@@ -4,9 +4,10 @@ package com.sherlocky.learning.java8.lambda;
  *  lambdas 来实现 Runnable 接口
  *
  *  <p>
- *  Lambda表达式并不是匿名内部类的语法糖，它是基于 invokedynamic 指令（java7引入的新指令），
+ *  Lambda表达式并不是匿名内部类的语法糖，它是基于 invokedynamic 指令（java7引入的新指令），不会产生新的class文件。
  *  在运行时使用 ASM 动态生成类文件来实现的，可以参考{@link java.lang.invoke.LambdaMetafactory#metafactory}。
  *  </p>
+ *  可参考：<a href="https://github.com/CarpenterLee/JavaLambdaInternals/blob/master/2-Lambda%20and%20Anonymous%20Classes(II).md">Lambda VS 内部类</a>
  *
  * @author zhangcx
  */
